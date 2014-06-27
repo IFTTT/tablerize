73606b280bffb52ff8e3a720c93ce1753685ecbe
Sean Zhu <sean.zhu@ifttt.com>
2014-06-27 15:49:23 -0700

    Support adding multiple classes

--- lib/kramdown-yaml-tablerize/yaml_tablerize.rb
+++ lib/kramdown-yaml-tablerize/yaml_tablerize.rb
@@ -40,8 +40,18 @@ module YamlTablerize
       @attrs.update opts[:attrs] unless opts[:attrs].nil?
     end
 
-    def add_class(klass)
-      return if klass.empty?
+    def add_class(classes)
+      if classes.respond_to? :each
+        classes.each do |klass|
+          add_single_class klass
+        end
+      else
+        add_single_class classes
+      end
+    end
+
+    def add_single_class(klass)
+      return if klass.nil? || klass.empty?
       classes = @attrs['class']
       classes << klass unless classes.include? klass
     end
